1f9a8e6868ebc5ce1f08a41eed15ebdfc5e1a9ff
Alex <alexboutakidis@gmail.com>
2021-04-05 14:59:52 +0200

    - added main fragments and navigation

---
+++ app/src/main/java/com/example/mtgportal/ui/InformationViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.mtgportal.ui
+
+import androidx.lifecycle.ViewModel
+
+class InformationViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/mtgportal/ui/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mtgportal.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.mtgportal.R
+import com.example.mtgportal.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavBar.setupWithNavController(navController)
+    }
+}
